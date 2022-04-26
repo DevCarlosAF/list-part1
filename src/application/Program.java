8f411b6cc9864b7b7ef6409dbec0d5b465bdf6ad
DevCarlosAF <carlosafdev@gmail.com>
2022-04-25 23:37:32 -0300

    Lambda expression implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,31 +34,24 @@ public class Program {
 		System.out.println();
 		System.out.print("Enter the employee id that will have salary increase: ");
 		int idsalary = input.nextInt();
-		Integer pos = position(list, idsalary);
-		if (pos == null) {
+		
+		Employee emp = list.stream().filter(x -> x.getId() == idsalary).findFirst().orElse(null);
+		
+		if (emp == null) {
 			System.out.println("This id does not exist!");
 		} else {
 			System.out.print("Enter the percentage: ");
 			double percentage = input.nextDouble();
-			list.get(pos).increaseSalary(percentage);
+			emp.increaseSalary(percentage);
 		}
 
 		System.out.println();
 		System.out.println("List of employees:");
-		for (Employee emp : list) {
-			System.out.println(emp);
+		for (Employee e : list) {
+			System.out.println(e);
 		}
 
 		input.close();
 
-	}
-
-	public static Integer position(List<Employee> list, int id) {
-		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).getId() == id) {
-				return i;
-			}
-		}
-		return null;
 	}
 }
